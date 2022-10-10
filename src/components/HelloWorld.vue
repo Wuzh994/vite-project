29d7bc5c12de3aad45015bee8185215b5e57b03d
wuzhenghong <2015129064@qq.com>
2022-10-10 20:22:56 +0800

    chore: cleanup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,4 @@
 <template>
   <h2>Vue 3.x + Vite 3.x + Pinia 2.x + Vue-Router 4.x</h2>
-  <p>ESLint, Stylelint, Prettier, Husky + lint-staged</p>
-  <span @click="handleClick">{{ num }}</span>
+  <p>ESLint, Stylelint, Prettier, Husky + lint-staged + commitlint</p>
 </template>
-
-<script setup>
-import { ref } from 'vue'
-
-const num = ref(0)
-
-if (num.value === 0) num.value = 5
-
-function handleClick () {
-  console.log(1111)
-}
-</script>
