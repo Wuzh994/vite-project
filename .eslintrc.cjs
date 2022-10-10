174bf44c553bc8978923e412de5311ef0f1dc174
wuzhenghong <2015129064@qq.com>
2022-10-10 16:19:42 +0800

    chore: add eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,5 +18,52 @@ module.exports = {
     'vue'
   ],
   rules: {
+    // import
+    'import/order': 'error',
+    'import/first': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-unresolved': 'off',
+    'import/no-absolute-path': 'off',
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: false
+      }
+    ],
+
+    // vue
+    // 'vue/attribute-hyphenation': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/require-prop-types': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+
+    'vue/attributes-order': ['error'],
+    'vue/component-tags-order': ['error', {
+      order: ['script', 'template', 'style']
+    }],
+    'vue/define-macros-order': ['error', {
+      order: ['defineProps', 'defineEmits']
+    }],
+    'vue/v-on-event-hyphenation': ['error', 'always', {
+      autofix: true
+    }],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ]
   }
 }
