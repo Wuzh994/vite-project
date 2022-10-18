2df2e48777a14f7eb8f416084a2cd27a5b59cd6f
wuzhenghong <2015129064@qq.com>
2022-10-18 10:36:22 +0800

    feat: add sass

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,3 +2,9 @@
   <h2>Vue 3.x + Vite 3.x + Pinia 2.x + Vue-Router 4.x</h2>
   <p>ESLint, Stylelint, Prettier, Husky + lint-staged + commitlint</p>
 </template>
+
+<style lang="scss" scoped>
+  p {
+    color: #999;
+  }
+</style>
